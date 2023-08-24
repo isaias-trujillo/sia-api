6dfd039d028d0818471301382f58917bfa6fab42
isaias-trujillo <119267321+isaias-trujillo@users.noreply.github.com>
2023-08-24 14:03:16 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json; charset=UTF-8');
+header('Access-Control-Allow-Methods: GET,POST,PUT,PATCH,DELETE');
+header('Access-Control-Allow-Headers: Content-Type,Access-Control-Allow-Headers,Authorization,X-Requested-With');
+
 require_once __DIR__."/src/autoload.php";
 
 use api\core\App;
@@ -7,5 +12,5 @@ use api\core\Endpoints;
 
 $root = basename(__DIR__)."/api";
 $app = new App($root);
-Endpoints::load($app);
+Endpoints::load($app);	
 $app();
