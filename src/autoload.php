109c4ca481b715d674b46143d5ae05630421ffab
isaias-trujillo <119267321+isaias-trujillo@users.noreply.github.com>
2023-08-30 12:36:20 -0500

    requiere file issue fixed

--- src/autoload.php
+++ src/autoload.php
@@ -1,12 +1,15 @@
 <?php
 
-function my_custom_autoloader( $class_name ){
-    $file = __DIR__.'/'.$class_name.'.php';
+function my_custom_autoloader($class_name)
+{
+    $file = __DIR__ . '/' . $class_name . '.php';
 
-    if ( file_exists($file) ) {
+    if (file_exists($file)) {
         require_once $file;
+    } else {
+        echo "File not found";
     }
 }
 
 // add a new autoloader by passing a callable into spl_autoload_register()
-spl_autoload_register( 'my_custom_autoloader' );
+spl_autoload_register('my_custom_autoloader');
